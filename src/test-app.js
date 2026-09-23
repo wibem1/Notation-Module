b67b85fa21db6bb4ebd38a696f56c8cd434f90d3
wibem1 <wibem1@googlemail.com>
2026-09-23 09:33:11 +0200

    Load v0.1.12 notation module

--- src/test-app.js
+++ src/test-app.js
@@ -1,4 +1,4 @@
-import {NotationModule} from './notation-module.js?v=0.1.11';
+import {NotationModule} from './notation-module.js?v=0.1.12';
 const $=id=>document.getElementById(id), status=$('status'), abc=$('abc');
 const ABC_STORAGE_KEY='notation-module.test-app.abc';
 const SCALE_STORAGE_KEY='notation-module.test-app.scale';
